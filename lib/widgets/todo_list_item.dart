71742e5f60c21c6a0a53f84ba9c5941630bd3b23
pbhoomi190 <pbhoomi190@gmail.com>
2020-07-15 14:57:11 +0530

    text to speech and speech to text

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:fluttertododemo/constants/constants.dart';
 import 'package:fluttertododemo/database/ToDo.dart';
 import 'package:fluttertododemo/constants/extensions.dart';
 import 'package:fluttertododemo/database/database_helper.dart';
 import 'package:fluttertododemo/language_support/localization_manager.dart';
+
+class ItemToDo {
+  ToDo toDo;
+  bool isPlaying;
+
+  ItemToDo({this.toDo, this.isPlaying});
+}
 
 class ToDoListItem extends StatefulWidget {
 
@@ -21,28 +29,31 @@ class ToDoListItem extends StatefulWidget {
 class _ToDoListItemState extends State<ToDoListItem> {
   DatabaseHelper helper = DatabaseHelper();
   bool isFav = false;
-  ToDo itemToDo;
+  ItemToDo itemToDo;
   String dateString = "";
   String image = allImage;
+  FlutterTts _flutterTts;
+  String txtToSpeak = "";
 
   manageFavourite() async {
-      var result = await helper.markFavouriteToDoItem(itemToDo, isFav ? 1 : 0);
+      var result = await helper.markFavouriteToDoItem(itemToDo.toDo, isFav ? 1 : 0);
       debugPrint(result.toString());
       setState(() {
-        itemToDo.isFavourite = isFav ? 1 : 0;
+        itemToDo.toDo.isFavourite = isFav ? 1 : 0;
       });
   }
 
   getDate() async {
-    if (itemToDo.date != 0) {
-      var date = await itemToDo.date.dateString();
+    if (itemToDo.toDo.date != 0) {
+      var date = await itemToDo.toDo.date.dateString();
       if (mounted) {
         setState(() {
           dateString = date;
         });
       }
+      txtToSpeak = "${itemToDo.toDo.title}, ${itemToDo.toDo.description}, $dateString";
     }
-    var cat = await itemToDo.category.getCategoryForId();
+    var cat = await itemToDo.toDo.category.getCategoryForId();
     if (mounted) {
       setState(() {
         image = cat.image;
@@ -50,11 +61,63 @@ class _ToDoListItemState extends State<ToDoListItem> {
     }
   }
 
+  initializeTts() {
+    _flutterTts = FlutterTts();
+
+    _flutterTts.setStartHandler(() {
+      setState(() {
+        itemToDo.isPlaying = true;
+      });
+    });
+
+    _flutterTts.setCompletionHandler(() {
+      setState(() {
+        itemToDo.isPlaying = false;
+      });
+    });
+
+    _flutterTts.setErrorHandler((err) {
+      setState(() {
+        print("error occurred: " + err);
+        itemToDo.isPlaying = false;
+      });
+    });
+  }
+
+  Future speak(String text) async {
+    if (text != null && text.isNotEmpty) {
+      var result = await _flutterTts.speak(text);
+      if (result == 1)
+        setState(() {
+          itemToDo.isPlaying = true;
+        });
+    }
+  }
+
+  Future stop() async {
+    var result = await _flutterTts.stop();
+    if (result == 1)
+      setState(() {
+        itemToDo.isPlaying = false;
+      });
+  }
+
+  void setTtsLanguage() async {
+    await _flutterTts.setLanguage("en-US");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _flutterTts.stop();
+  }
+
   @override
   void initState() {
     isFav = widget.toDo.isFavourite == 0 ? false : true;
-    itemToDo = widget.toDo;
+    itemToDo = ItemToDo(toDo: widget.toDo, isPlaying: false);
     getDate();
+    initializeTts();
     super.initState();
   }
 
@@ -82,9 +145,9 @@ class _ToDoListItemState extends State<ToDoListItem> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(itemToDo.title, maxLines: 1, overflow: TextOverflow.ellipsis,),
+                        Text(itemToDo.toDo.title, maxLines: 1, overflow: TextOverflow.ellipsis,),
                         const SizedBox(height: 5,),
-                        Text(itemToDo.description, maxLines: 1, overflow: TextOverflow.ellipsis,),
+                        Text(itemToDo.toDo.description, maxLines: 1, overflow: TextOverflow.ellipsis,),
                         const SizedBox(height: 5,),
                         Text(dateString, maxLines: 1, overflow: TextOverflow.ellipsis,),
                         const SizedBox(height: 5,),
@@ -92,9 +155,19 @@ class _ToDoListItemState extends State<ToDoListItem> {
                     ),
                   ),
                   const SizedBox(width: 8,),
+                  IconButton(
+                    icon: itemToDo.isPlaying ? Icon(Icons.stop) : Icon(Icons.record_voice_over),
+                    onPressed: () {
+                      if (itemToDo.isPlaying) {
+                        stop();
+                      } else {
+                        speak(txtToSpeak);
+                      }
+                    },
+                  ),
                   Semantics(
                     label: obj.getTranslatedValue("swipe_option_talkback"),
-                    selected: itemToDo.isFavourite == 0 ? false : true,
+                    selected: itemToDo.toDo.isFavourite == 0 ? false : true,
                     onTap: () {
                       widget.onFavClick();
                       isFav = !isFav;
@@ -105,7 +178,7 @@ class _ToDoListItemState extends State<ToDoListItem> {
                       height: 100,
                       child: IconButton(
                         tooltip: "favorite",
-                        icon: Icon(itemToDo.isFavourite == 0 ? Icons.favorite_border : Icons.favorite , color: Theme.of(context).primaryColor,),
+                        icon: Icon(itemToDo.toDo.isFavourite == 0 ? Icons.favorite_border : Icons.favorite , color: Theme.of(context).primaryColor,),
                         onPressed: () {
                           widget.onFavClick();
                           isFav = !isFav;
